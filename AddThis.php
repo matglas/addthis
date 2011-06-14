cfab0d33378844eb0182bda4e65ca585db2cba2e
Jani Palsamäki <jani.palsamaki@gmail.com>
2011-06-14 13:50:20 +0300

    - Renamed AddThis username to profile ID, as this is more consistent with the current API described at addthis.com - Moved some literals to AddThis-class as constants to reduce literal duplication

--- AddThis.php
+++ AddThis.php
@@ -9,6 +9,8 @@
 class AddThis {
 
   const MODULE_NAME = 'addthis';
+  const PROFILE_ID_KEY = 'addthis_profile_id';
+  const WIDGET_TYPE_KEY = 'addthis_block_widget_type';
   const WIDGET_TYPE_DISABLED = 'disabled';
   const WIDGET_TYPE_COMPACT_BUTTON = 'compact_button';
   const WIDGET_TYPE_LARGE_BUTTON = 'large_button';
@@ -26,33 +28,33 @@ class AddThis {
   }
 
   public static function getWidgetType() {
-    return variable_get('addthis_block_widget_type', self::WIDGET_TYPE_COMPACT_BUTTON);
+    return variable_get(self::WIDGET_TYPE_KEY, self::WIDGET_TYPE_COMPACT_BUTTON);
   }
 
   public static function getWidgetMarkup($widgetType = '') {
-    $username = self::getUsername();
+    $profileId = self::getProfileId();
     switch ($widgetType) {
       case self::WIDGET_TYPE_LARGE_BUTTON:
         $markup = sprintf(
-          '<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=%s"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=%s"></script>',
-          $username, $username
+          '<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;pubid=%s"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=%s"></script>',
+          $profileId, $profileId
         );
         break;
       case self::WIDGET_TYPE_COMPACT_BUTTON:
         $markup = sprintf(
-          '<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=%s"><img src="http://s7.addthis.com/static/btn/sm-share-en.gif" width="83" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=%s"></script>',
-          $username, $username
+          '<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;pubid=%s"><img src="http://s7.addthis.com/static/btn/sm-share-en.gif" width="83" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=%s"></script>',
+          $profileId, $profileId
         );
         break;
       case self::WIDGET_TYPE_TOOLBOX:
         $markup = sprintf(
-          '<div class="addthis_toolbox addthis_default_style"><a href="http://www.addthis.com/bookmark.php?v=250&amp;username=%s" class="addthis_button_compact">Share</a><span class="addthis_separator">|</span><a class="addthis_button_preferred_1"></a><a class="addthis_button_preferred_2"></a><a class="addthis_button_preferred_3"></a><a class="addthis_button_preferred_4"></a></div><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=%s"></script>',
-          $username, $username
+          '<div class="addthis_toolbox addthis_default_style"><a href="http://www.addthis.com/bookmark.php?v=250&amp;pubid=%s" class="addthis_button_compact">Share</a><span class="addthis_separator">|</span><a class="addthis_button_preferred_1"></a><a class="addthis_button_preferred_2"></a><a class="addthis_button_preferred_3"></a><a class="addthis_button_preferred_4"></a></div><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=%s"></script>',
+          $profileId, $profileId
         );
         break;
       case self::WIDGET_TYPE_SHARECOUNT:
-        $markup = sprintf('<div class="addthis_toolbox addthis_default_style"><a class="addthis_counter"></a></div><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=%s"></script>',
-          $username
+        $markup = sprintf('<div class="addthis_toolbox addthis_default_style"><a class="addthis_counter"></a></div><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=%s"></script>',
+          $profileId
         );
         break;
       default:
@@ -62,7 +64,7 @@ class AddThis {
     return $markup;
   }
 
-  private static function getUsername() {
-    return variable_get('addthis_username', '');
+  public static function getProfileId() {
+    return variable_get(AddThis::PROFILE_ID_KEY, '');
   }
 }
