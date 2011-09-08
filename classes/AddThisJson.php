d4bb00ca804aa8a5203c0744effd9ce43f820f87
Jani Palsamäki <jani.palsamaki@gmail.com>
2011-09-08 12:19:59 +0300

    Minor refactoring: Using drupal_json_decode instead of json_decode.

--- classes/AddThisJson.php
+++ classes/AddThisJson.php
@@ -11,6 +11,6 @@ class AddThisJson {
   public function decode($url) {
     $response = drupal_http_request($url);
     $responseOk = $response->code == 200;
-    return $responseOk ? json_decode($response->data, TRUE) : NULL;
+    return $responseOk ? drupal_json_decode($response->data) : NULL;
   }
 }
