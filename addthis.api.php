761ee2eed1e8cf82cfaa1298e63abcaed4656839
Matthias Glastra <matglas.git@gmail.com>
2012-05-07 20:47:45 +0200

    Added some examples to the api file.

--- addthis.api.php
+++ addthis.api.php
@@ -19,7 +19,19 @@
  */
 function hook_addthis_markup_options_alter(&$options) {
   global $base_root;
+
+  // Change the url used on the share buttons.
   $options['#url'] = $base_root . request_uri();
+
+  // To apply different service this to the block implementation try this.
+  if (isset($options['#block']) && $options['#display']['type'] == 'addthis_basic_toolbox') {
+
+    // Change the var below to add other services.
+    $displayed_services = 'twitter,google_plusone,facebook';
+    $options['#display']['settings']['share_services'] = $displayed_services;
+    $options['#display']['settings']['buttons_size'] = AddThis::CSS_16x16;
+
+  }
 }
 
 /**
@@ -34,5 +46,10 @@ function hook_addthis_markup_alter(&$markup) {
   // twitter button, so we can change the look.
   if (!empty($markup['twitter'])) {
     $markup['twitter']['#attributes']['class'][] = "custom_twitter_class";
+  }
+
+  // Or change button size for Google +1 for example.
+  if (!empty($markup['google_plusone'])) {
+    $markup['google_plusone']['#attributes']['g:plusone:size'] = 'small';
   }
 }
