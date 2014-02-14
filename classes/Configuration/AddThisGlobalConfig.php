6e5c55bcd55df4bda5e29b8eb70ac0c105166339
Matthias Glastra <matglas.git@gmail.com>
2014-02-14 09:18:54 +0100

    Added global config class. First stab at profile id.

--- classes/Configuration/AddThisGlobalConfig.php
+++ classes/Configuration/AddThisGlobalConfig.php
@@ -1,9 +1,39 @@
 <?php
+/**
+ * @file
+ * AddThis configuration for the Global settings.
+ */
 
-class AddThisGlobalConfig {
+class AddThisGlobalConfig extends AddThisConfig {
 
-  function __construct() {
-    drupal_set_message(t('it works'), 'status', FALSE);
+  /**
+   * Construct the Global configuration class.
+   */
+  public function __construct() {
+    parent::__construct();
+
+    self::addConfiguration(self::getProfileIdKey(), '');
+  }
+
+  /**
+   * Get the Profile ID.
+   */
+  public function getProfileId() {
+    return self::getValue(self::getProfileIdKey());
+  }
+
+  /**
+   * Set the Profile ID.
+   */
+  public function setProfileId($profile_id) {
+    return self::setValue(self::getProfileIdKey(), $profile_id);
+  }
+
+  /**
+   * Get the configuration key Profile ID.
+   */
+  public function getProfileIdKey() {
+    return 'addthis_profile_id';
   }
 
 }
